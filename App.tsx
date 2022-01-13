fb3558ca1e4a96d4dd39b862d8f42ce6f390d9c5
Milena Hikari <milenahikari11@gmail.com>
2022-01-13 20:32:14 -0300

    feat: configurando o tema no projeto

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { View } from "react-native";
 import AppLoading from "expo-app-loading";
 import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
+import { ThemeProvider } from "styled-components/native";
+
+import theme from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts([
@@ -13,5 +15,5 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />;
   }
-  return <View></View>;
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 }
