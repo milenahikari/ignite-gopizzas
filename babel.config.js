579b9bcfea350b3b64527eac80f291801554edb7
Milena Hikari <milenahikari11@gmail.com>
2022-01-13 20:04:40 -0300

    feat: adicionando babel module resolver para path mapping das importacoes

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: [["babel-preset-expo", { jsxRuntime: "automatic" }]],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ts", ".tsx", ".js", ".json"],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@contexts": "./src/contexts",
+            "@pages": "./src/pages",
+            "@routes": "./src/routes",
+            "@services": "./src/services",
+            "@styles": "./src/styles",
+            "@utils": "./src/utils",
+            "@src": "./src",
+          },
+        },
+      ],
+    ],
   };
 };
