6813ece78f518703d6bdae395a3f1a29407961db
Milena Hikari <milenahikari11@gmail.com>
2022-01-19 21:20:57 -0300

    feat: finalizando tela de login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
+
+import brandImg from "@assets/brand.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
@@ -8,16 +11,29 @@ import * as S from "./styles";
 export function SignIn() {
   return (
     <S.Container>
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <S.Content>
+          <S.Brand source={brandImg} />
 
-      <Input placeholder="Senha" type="secondary" secureTextEntry />
+          <S.Title>Login</S.Title>
 
-      <Button title="Entrar" type="secondary" />
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+
+          <Input placeholder="Senha" type="secondary" secureTextEntry />
+
+          <S.ForgotPasswordButton>
+            <S.ForgotPasswordLabel>Esqueci minha senha</S.ForgotPasswordLabel>
+          </S.ForgotPasswordButton>
+          <Button title="Entrar" type="secondary" />
+        </S.Content>
+      </KeyboardAvoidingView>
     </S.Container>
   );
 }
