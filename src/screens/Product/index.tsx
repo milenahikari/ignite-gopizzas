898373f7212b32896f43f92861f5903c6fa10581
Milena Hikari <milenahikari11@gmail.com>
2022-01-23 15:46:58 -0300

    feat: criando header de produto

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { Platform } from "react-native";
+import { Platform, TouchableOpacity } from "react-native";
 
 import * as S from "./styles";
 
 export function Product() {
   return (
-    <S.Container
-      behavior={Platform.OS === "ios" ? "padding" : undefined}
-    ></S.Container>
+    <S.Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
+      <S.Header>
+        <S.Title>Cadastrar</S.Title>
+
+        <TouchableOpacity>
+          <S.DeleteLabel>Deletar</S.DeleteLabel>
+        </TouchableOpacity>
+      </S.Header>
+    </S.Container>
   );
 }
--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -1,8 +1,38 @@
 import styled, { css } from "styled-components/native";
+import { LinearGradient } from "expo-linear-gradient";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.KeyboardAvoidingView`
   ${({ theme }) => css`
     flex: 1;
     background-color: ${theme.COLORS.BACKGROUND};
+  `}
+`;
+
+export const Header = styled(LinearGradient).attrs(({ theme }) => ({
+  colors: theme.COLORS.GRADIENT,
+}))`
+  width: 100%;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: ${getStatusBarHeight() + 33}px 20px 24px;
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: 24px;
+    font-family: ${theme.FONTS.TITLE};
+    color: ${theme.COLORS.TITLE};
+  `}
+`;
+
+export const DeleteLabel = styled.Text`
+  ${({ theme }) => css`
+    font-size: 14px;
+    font-family: ${theme.FONTS.TEXT};
+    color: ${theme.COLORS.TITLE};
   `}
 `;
