6f0975127994a7d4d4531e56595d00cedf82f121
Milena Hikari <milenahikari11@gmail.com>
2022-01-23 15:54:57 -0300

    feat: criando componente button back

---
+++ src/components/ButtonBack/styles.ts
@@ -0,0 +1,13 @@
+import styled, { css } from "styled-components/native";
+
+export const Container = styled.TouchableOpacity`
+  ${({ theme }) => css`
+    width: 40px;
+    height: 40px;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 12px;
+    border: 1px solid ${theme.COLORS.PRIMARY_100};
+  `}
+`;
--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Platform, TouchableOpacity } from "react-native";
 
+import { ButtonBack } from "@components/ButtonBack";
+
 import * as S from "./styles";
 
 export function Product() {
   return (
     <S.Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <S.Header>
+        <ButtonBack />
+
         <S.Title>Cadastrar</S.Title>
 
         <TouchableOpacity>
