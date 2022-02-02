e872f5dc614ce7fee2a49646b7a7d8e150b9cc9f
Milena Hikari <milenahikari11@gmail.com>
2022-02-02 20:27:15 -0300

    feat: criando formulario para cadastro

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { Platform, TouchableOpacity } from "react-native";
+import { Platform, TouchableOpacity, ScrollView } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
 import { ButtonBack } from "@components/ButtonBack";
 import { Photo } from "@components/Photo";
 import { InputPrice } from "@components/InputPrice";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 import * as S from "./styles";
 
@@ -27,29 +29,53 @@ export function Product() {
   }
   return (
     <S.Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
-      <S.Header>
-        <ButtonBack />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <S.Header>
+          <ButtonBack />
 
-        <S.Title>Cadastrar</S.Title>
+          <S.Title>Cadastrar</S.Title>
 
-        <TouchableOpacity>
-          <S.DeleteLabel>Deletar</S.DeleteLabel>
-        </TouchableOpacity>
-      </S.Header>
+          <TouchableOpacity>
+            <S.DeleteLabel>Deletar</S.DeleteLabel>
+          </TouchableOpacity>
+        </S.Header>
 
-      <S.Upload>
-        <Photo uri={image} />
+        <S.Upload>
+          <Photo uri={image} />
 
-        <S.PickImageButton
-          title="Carregar"
-          type="secondary"
-          onPress={handlePickerImage}
-        />
-      </S.Upload>
+          <S.PickImageButton
+            title="Carregar"
+            type="secondary"
+            onPress={handlePickerImage}
+          />
+        </S.Upload>
 
-      <InputPrice size="P" />
-      <InputPrice size="M" />
-      <InputPrice size="G" />
+        <S.Form>
+          <S.InputGroup>
+            <S.Label>Nome</S.Label>
+            <Input />
+          </S.InputGroup>
+
+          <S.InputGroup>
+            <S.InputGroupHeader>
+              <S.Label>Descrição</S.Label>
+              <S.MaxCharacters>0 de 60 caracteres</S.MaxCharacters>
+            </S.InputGroupHeader>
+
+            <Input multiline maxLength={60} style={{ height: 80 }} />
+          </S.InputGroup>
+
+          <S.InputGroup>
+            <S.Label>Tamanhos e preços</S.Label>
+
+            <InputPrice size="P" />
+            <InputPrice size="M" />
+            <InputPrice size="G" />
+          </S.InputGroup>
+
+          <Button title="Cadastrar pizza" />
+        </S.Form>
+      </ScrollView>
     </S.Container>
   );
 }
